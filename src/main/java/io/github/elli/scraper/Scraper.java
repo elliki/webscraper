87652713fa247f1a01c8ba2c5330c0ac314ec0ae
elliki <weigel.ele@gmail.com>
2021-10-17 14:58:15 +0200

    webscraper now iterating all links recursively

--- src/main/java/io/github/elli/scraper/Scraper.java
+++ src/main/java/io/github/elli/scraper/Scraper.java
@@ -7,53 +7,80 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 public class Scraper {
-
-    static List<String> linkVisited = new ArrayList<String>();
-    static List<String> linkNew = new ArrayList<String>();
-    static List<WebElement> zwischenListe =new ArrayList<WebElement>();
     public static final Logger LOGGER = LoggerFactory.getLogger(Scraper.class);
+
+    private Set<String> linkVisited = new HashSet<>();
+    private WebDriver driver;
+
+    public Scraper() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
 
     public void run(String[] args) throws IOException, URISyntaxException {
         JsonParser parser = new JsonParser();
         Websites websites = parser.parse();
-        linkNew.addAll(websites.getWebsites());
-        firstScrape(linkNew);
+        try {
+            scrapeLinksRecursively(websites.getWebsites());
+        } catch (InterruptedException e) {
+            LOGGER.error("Thread got interrupted", e);
+            Thread.currentThread().interrupt();
+        }
     }
 
-    public void firstScrape(List<String> website) {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        for (String test: website) {
-            zwischenListe.addAll(driver.findElements(By.tagName("a")));
-            driver.get(test);
-            zwischenListe.addAll(driver.findElements(By.tagName("a")));
-            linkVisited.add(test);
+    public void scrapeLinksRecursively(List<String> websiteLinks) throws InterruptedException {
+        if(websiteLinks == null || websiteLinks.isEmpty()) {
+            return;
         }
-        linkNew.clear();
-        iterator(zwischenListe);
-        zwischenListe.clear();
+
+        List<String> zwischenListe = new ArrayList<>();
+        for (String link: websiteLinks) {
+            driver.get(link);
+            Thread.sleep(50);
+            zwischenListe.addAll(driver.findElements(By.tagName("a")).stream()
+                    .map(this::extractHref)
+                    .collect(Collectors.toList()));
+            linkVisited.add(link);
+        }
+
+        List<String> newLinks = zwischenListe.stream()
+                .filter(this::hasLinkNotBeenVisited)
+                .filter(Objects::nonNull)
+                .filter(this::isNotExternalLink)
+                .collect(Collectors.toList());
+
+        scrapeLinksRecursively(newLinks);
     }
 
-    private void iterator(List<WebElement> zwischenListe) {
-        for (WebElement test : zwischenListe) {
-            if (!linkVisited.contains(test.getAttribute("href")) && test.getAttribute("href").contains("https://www.mitkids-unterwegs.de/")) {
-                linkNew.add(test.getAttribute("href"));
-                break;
-            }
+    private boolean isNotExternalLink(String link) {
+        return link.contains("https://www.mitkids-unterwegs.de/") ||  link.contains("https://www.karlsruhe.de");
+    }
+
+    private String extractHref(WebElement webElement) {
+        try {
+            System.out.println(webElement.getAttribute("href"));
+            return webElement.getAttribute("href");
         }
-        firstScrape(linkNew);
+        catch (Exception e) {
+            LOGGER.warn("could not extract link from element. Error was:");
+            return null;
+        }
+
+    }
+
+    private boolean hasLinkNotBeenVisited(String link) {
+        return !linkVisited.contains(link);
     }
 }
 
